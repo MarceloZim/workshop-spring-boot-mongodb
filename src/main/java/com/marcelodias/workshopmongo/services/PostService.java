f4d53e35c54b69ed6ddf9477a6e1be473f2a4432
Marcelo Dias <marcelod232015@gmail.com>
2023-09-20 23:13:41 -0300

    Obtendo um post por id

---
+++ src/main/java/com/marcelodias/workshopmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package com.marcelodias.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcelodias.workshopmongo.domain.Post;
+import com.marcelodias.workshopmongo.repository.PostRepository;
+import com.marcelodias.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
